2473da5584f634789ccb884e5b481de4ea6d4b6e
Anda Sizani <139394188+AndaSizaniZazu@users.noreply.github.com>
2024-10-24 22:29:19 +0200

    Add files via upload

--- ClientService.cs
+++ ClientService.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DeveloperAssessment
 {
-    internal class ClientService
+    public class ClientService
     {
+        private const string FilePath = "clients.json";
+
+        public void SaveClient(Client client)
+        {
+            var clients = LoadClients();
+            clients.Add(client);
+            SaveClients(clients);
+        }
+        public List<Client> LoadClients()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return new List<Client>();
+            }
+
+            var json = File.ReadAllText(FilePath);
+            return JsonSerializer.Deserialize<List<Client>>(json) ?? new List<Client>();
+        }
+
+        private void SaveClients(List<Client> clients)
+        {
+            var json = JsonSerializer.Serialize(clients);
+            File.WriteAllText(FilePath, json);
+        }
     }
 }
