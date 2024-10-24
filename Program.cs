04aed75dc2328abdddbe8290bf5a80a1966a0d75
Anda Sizani <139394188+AndaSizaniZazu@users.noreply.github.com>
2024-10-24 22:29:57 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using DeveloperAssessment;
+
+static void Main(string[] args)
+{
+    var repository = new ClientService();
+    var client = CaptureClientDetails();
+    repository.SaveClient(client);
+    Console.WriteLine("Client details saved successfully!");
+}
+static Client CaptureClientDetails()
+{
+    var client = new Client();
+
+    Console.WriteLine("Capture Client Details");
+
+    client.ClientName = PromptForInput("Enter Client Name: ");
+    client.DateRegistered = DateTime.Parse(PromptForInput("Enter Date Registered (yyyy-mm-dd): "));
+    client.Location = PromptForInput("Enter Location: ");
+    client.NumberOfUsers = int.Parse(PromptForInput("Enter Number of Users: "));
+
+    return client;
+}
+static string PromptForInput(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
